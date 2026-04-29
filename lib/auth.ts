d38c03e0a212574439f668c1bc2e23ad8794f939
Akshat Kankani <157486069+kankaniakshat185@users.noreply.github.com>
2026-04-30 02:04:35 +0530

    added auth env variables

--- lib/auth.ts
+++ lib/auth.ts
@@ -11,6 +11,12 @@ export const auth = betterAuth({
     emailAndPassword: {
         enabled: true,
     },
+    socialProviders: {
+        google: {
+            clientId: process.env.GOOGLE_CLIENT_ID || "",
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
+        },
+    },
     baseURL: process.env.BETTER_AUTH_URL || "https://biasscope-app.vercel.app",
     trustedOrigins: ["https://biasscope-app.vercel.app", "http://localhost:3000"],
     secret: process.env.BETTER_AUTH_SECRET || "default_super_secret_key"
